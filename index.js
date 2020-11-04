4054cf8d712e166cbfbbe60a85062255205aee8f
VernalEquinox <pieroapruzzese@gmail.com>
2020-11-04 16:57:52 +0100

    ex-5 somewhere

--- index.js
+++ index.js
@@ -14,16 +14,36 @@ document.getElementById("but").onclick=callJavascriptFunction; */
 const hideToggle = function () {
     let allCards = document.querySelectorAll(".col-md-4 > .card")
     let hideButtons = document.querySelectorAll(".card-body button:last-of-type")
-    for (let i = 0; i < hideButtons.length; i++) {
-        if (hideButtons[i].onclick) {
-        
-                allCards[i].style.display = "none";
+    /* for (let i = 0; i < hideButtons.length; i++) {
+        (function (index) {
+            hideButtons[i].onclick = function () {
+                alert(index)
             }
-        } 
+
+        })(i) */
+    
+    
+
+    for (let i = 0; i < hideButtons.length; i++)
+{
+
+    (function(index){
+        hideButtons[i].onclick = function(){
+            allCards[index].style.display="none";
+        }    
+    })(i);
+
+}
+
+
+        
+               /*  allCards[i].style.display = "none"; */
+            }
+/*         } 
         
 
             
-        }
+        } */
     /* console.log(allCards) */
 
     
